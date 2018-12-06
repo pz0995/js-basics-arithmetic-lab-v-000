535fa0c32a2d6a2a1b288c71af2c57ac4b60ab58
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-06 05:08:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+function newID(oldID, newID) {
+  oldID + 1000000000;
+}
+
+function ageIsValid() {
+  if (Number.isInteger(currentAge))
+    return true;
+}
+
+function randomNumber() {
+  Math.random() * 20;
+}
+
+function randomInteger(randomInteger) {
+  randomInteger = Math.floor(Math.random() * 20) + 1;
+;
+function randomUserID() {
+  var randomUserID = randomInteger + 1000000000;
+}
